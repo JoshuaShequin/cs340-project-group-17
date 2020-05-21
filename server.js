b4f5e7aa5597073945202f7f4e58b5af3a5ed845
shequin-joshua-9147 <joshua.shequin.9147@mail.linnbenton.edu>
2020-05-21 02:53:30 -0700

    Adding js server hosting paired with handlebars templating

---
+++ server.js
@@ -0,0 +1,83 @@
+// imports
+var express = require('express');
+var process = require('process');
+var exphbs = require("express-handlebars")
+var fs = require('fs');
+
+var app = express();
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+
+
+//////Start/////////////--File Hosting--///////////////////////////////////////////////
+
+app.use(express.static('public')); // any files in public can be requested and will be returned.
+
+app.get('/', function(req, res, next){
+	// set our default page to index.html, served through handlebars
+	res.status(200).render('index');
+});
+
+app.get('/color', function(req, res, next){
+	// set our default page to index.html, served through handlebars
+	res.status(200).render('color');
+});
+
+app.get('/createaccount', function(req, res, next){
+	// set our default page to index.html, served through handlebars
+	res.status(200).render('createaccount');
+});
+
+app.get('/createtest', function(req, res, next){
+
+	res.status(200).render('createtests');
+});
+
+app.get('/findtests', function(req, res, next){
+
+	res.status(200).render('findtests');
+});
+
+app.get('/login', function(req, res, next){
+
+	res.status(200).render('login');
+});
+
+app.get('/managetest', function(req, res, next){
+
+	res.status(200).render('managetest');
+});
+
+app.get('/manageuser', function(req, res, next){
+
+	res.status(200).render('login');
+});
+
+app.get('/question', function(req, res, next){
+
+	res.status(200).render('login');
+});
+
+app.get('*', function (req, res, next){
+	// if requested routing does not exist, serve 404 page through handlebars
+	res.status(404).render('404');
+});
+
+//////End///////////////--File Hosting--///////////////////////////////////////////////
+
+/////--SERVER START--//////////////////////////
+
+SERVER_PORT = process.env.PORT; // this is the port that the server will listen on.
+// set with PORT environment variable.
+
+if (SERVER_PORT == undefined){
+	// If the PORT variable does not exist, default to port 8000
+	SERVER_PORT = 8000;
+}
+
+app.listen(SERVER_PORT, function (){
+	console.log("Server has started and is listening on port " + SERVER_PORT);
+})
