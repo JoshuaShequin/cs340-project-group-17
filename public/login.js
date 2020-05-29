fd0361d8c438f93221865517283657f36b7b8de2
shequin-joshua-9147 <joshua.shequin.9147@mail.linnbenton.edu>
2020-05-29 03:12:20 -0700

    Finishing the tasks to allow the ability to login and for the site to require a login to use it.

--- public/login.js
+++ public/login.js
@@ -1,5 +1,38 @@
+function hideModals() {
+
+  var showSomethingModal = document.getElementById('create-login-error-modal');
+
+  var modalBackdrop = document.getElementById('modal-backdrop');
+
+  showSomethingModal.classList.add('hidden');
+
+  modalBackdrop.classList.add('hidden');
+
+};
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function reqListener(){
-	console.log(this.responseText);
+	if (this.responseText == "Login successful!"){
+		setCookie("user_name",document.getElementById('username-input').value.trim(), 5);
+		setCookie("pass", document.getElementById('password-input').value.trim(), 5);
+		
+		window.location.replace("/home");
+	}
+	else{
+		var showSomethingModal = document.getElementById('create-login-error-modal');
+		var modalBackdrop = document.getElementById('modal-backdrop');
+
+		showSomethingModal.classList.remove('hidden');
+		modalBackdrop.classList.remove('hidden');
+		setCookie("user_name",'', 5);
+		setCookie("pass", '', 5);
+	};
 }
 
 function hitLogin(){
@@ -21,6 +54,11 @@ window.addEventListener('DOMContentLoaded', function () {
 
   var loginButton = document.getElementById('login-button');
   loginButton.addEventListener('click', hitLogin);
+  
+  var modalHideButtons = document.getElementsByClassName('modal-exit-button');
+	for (var i = 0; i < modalHideButtons.length; i++) {
+		modalHideButtons[i].addEventListener('click', hideModals);
+	}
 
 });
 
