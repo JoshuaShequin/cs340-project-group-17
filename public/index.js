498e219ad8e1037f5e9d37095c1d1904aab2040d
shequin-joshua-9147 <joshua.shequin.9147@mail.linnbenton.edu>
2020-06-03 21:25:18 -0700

    Adding support for the manage tests page, still can't manage specific tests

--- public/index.js
+++ public/index.js
@@ -5,6 +5,31 @@ function setCookie(cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
+
+
+
+function attach_url_to_manage_button(){
+	content_of_button = event.target.value.split(",");
+	test_id = content_of_button[0];
+	
+	window.location.replace("/managetest/"+getCookie("user_name")+"/"+test_id);
+};
+
 
 function logout(){
 	setCookie("user_name",'', 5);
@@ -17,5 +42,10 @@ window.addEventListener('DOMContentLoaded', function () {
 	
 	var logoutButton = document.getElementById('logout-button');
 	logoutButton.addEventListener('click', logout);
+	
+	var manage_tests_buttons = document.getElementsByClassName('Manage-Test-Button');
+	for (var i = 0; i < manage_tests_buttons.length; i++){
+		manage_tests_buttons[i].addEventListener('click', attach_url_to_manage_button);
+	}
 	
 });
