a2bb61f0e3a3faa079a7a1346ef07c0b0bd4710f
Beniamin Condrea <benjamin.condrea@gmail.com>
2020-06-08 02:07:39 -0700

    Added take test in search

--- public/findTestColumnTemplate.js
+++ public/findTestColumnTemplate.js
@@ -18,6 +18,8 @@ templates['findTestColumn'] = template({"compiler":[8,">= 4.3.0"],"main":functio
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":5,"column":8},"end":{"line":5,"column":16}}}) : helper)))
     + "</td>\r\n    <td>"
     + alias4(((helper = (helper = lookupProperty(helpers,"user_name") || (depth0 != null ? lookupProperty(depth0,"user_name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"user_name","hash":{},"data":data,"loc":{"start":{"line":6,"column":8},"end":{"line":6,"column":21}}}) : helper)))
-    + "</td>\r\n</tr>";
+    + "</td>\r\n    <td><button class=\"test-information\" type=\"submit\" onclick=\"location.href='/testInformation/"
+    + alias4(((helper = (helper = lookupProperty(helpers,"test_ID") || (depth0 != null ? lookupProperty(depth0,"test_ID") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"test_ID","hash":{},"data":data,"loc":{"start":{"line":7,"column":96},"end":{"line":7,"column":107}}}) : helper)))
+    + "'\">Take Test</button></td>\r\n</tr>";
 },"useData":true});
 })();
